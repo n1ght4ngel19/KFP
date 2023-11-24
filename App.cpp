82e2b3bd0de0b8919fec4da44fda7dcbf013144d
Levente Hadhazi <levente.hadhazi@ni.com>
2023-11-24 18:55:47 +0100

    Successfully finding hand contours in images

--- App.cpp
+++ App.cpp
@@ -6,17 +6,55 @@
 int main() {
 	std::cout << "Hello OpenCV " << CV_VERSION << std::endl;
 
-	cv::Mat image;
-	image = cv::imread("images/IMG_20231123_233211.jpg", cv::IMREAD_COLOR);
+	// Load etalon images
+	cv::Mat etalonRock;
+	etalonRock = cv::imread("images/Rock.jpg", cv::IMREAD_COLOR);
 
-	if (image.empty()) {
+	if (etalonRock.empty()) {
 		std::cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
 
-	cv::resize(image, image, cv::Size(), 0.15, 0.15);
+	cv::resize(etalonRock, etalonRock, cv::Size(), 0.15, 0.15);
 
-	cv::imshow("img", image);
+	// Resize images to fit screen
+	cv::Mat grayEtalonRock;
+	cv::cvtColor(etalonRock, grayEtalonRock, cv::COLOR_BGR2GRAY);
+
+	cv::Mat threshEtalonRock;
+
+	cv::adaptiveThreshold(grayEtalonRock, threshEtalonRock, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 11);
+
+	cv::bitwise_not(threshEtalonRock, threshEtalonRock);
+
+	cv::GaussianBlur(threshEtalonRock, threshEtalonRock, cv::Size(3, 3), 0, 0);
+
+	cv::imshow("gray", grayEtalonRock);
+	cv::imshow("thresh", threshEtalonRock);
+
+	std::vector<std::vector<cv::Point>> contours;
+	cv::findContours(threshEtalonRock, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+
+
+
+	cv::Mat drawing = cv::Mat::zeros(threshEtalonRock.size(), CV_8UC3);
+
+	int largest_area = 0;
+	int largest_contour_index = 0;
+
+	for (size_t i = 0; i < contours.size(); i++) {
+		double area = cv::contourArea(contours[i]);
+
+		if (area > largest_area) {
+			largest_area = area;
+			largest_contour_index = i;
+		}
+	}
+
+	cv::Scalar color = cv::Scalar(0, 0, 255);
+	cv::drawContours(drawing, contours, largest_contour_index, color, 1, cv::LINE_8);
+
+	imshow("Contours", drawing);
 
 	cv::waitKey(0);
 
