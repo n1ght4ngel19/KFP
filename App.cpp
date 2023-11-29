a3604e6f0d07906c691ef8369fd8055128aa13d2
Levente Hadhazi <levente.hadhazi@ni.com>
2023-11-29 23:18:11 +0100

    Finished project

--- App.cpp
+++ App.cpp
@@ -4,23 +4,21 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-cv::Mat colorRead(std::string directory, std::string fileName) {
-	std::string path = "images/" + directory + "/" + fileName + ".jpg";
+namespace fs = std::filesystem;
 
-	std::cout << "Reading " << path << std::endl;
+const int LINE_THICKNESS = 40;
+// The similarity threshold for a match to be accepted as possibly valid
+// This is adjusted when the program detects false positives
+double ACCEPTANCE_THRESHOLD = 0.1;
+int MAX_RETRIES = 5;
+bool RUN_VERBOSE = true;
 
-	cv::Mat image = cv::imread(path, cv::IMREAD_COLOR);
-
-	cv::waitKey(0);
-
-	if (image.empty()) {
-		std::cout << "Could not open or find the image (Reading)" << std::endl;
-
-		return cv::Mat();
-	}
-
-	return image;
-}
+enum Gesture {
+	NONE,
+	PAPER,
+	ROCK,
+	SCISSORS
+};
 
 int findLargestContour(std::vector<std::vector<cv::Point>> contours) {
 	int largestArea = 0;
@@ -38,137 +36,375 @@ int findLargestContour(std::vector<std::vector<cv::Point>> contours) {
 	return largestContourIdx;
 }
 
-cv::Mat makeDrawing(cv::Mat image, std::string drawingName, bool doResize, cv::Scalar color) {
-	if (doResize) {
-		cv::resize(image, image, cv::Size(), 0.15, 0.15);
-	}
+double matchingOf(cv::Mat drawing1, cv::Mat drawing2) {
+	// matchShapes() is independent of the scale, rotation, and starting point of the contour,
+	// so we don't need to adjust the images for these factors
+	float match = cv::matchShapes(drawing1, drawing2, cv::CONTOURS_MATCH_I3, 0);
+	//std::cout << "Match (0-1): " << match << std::endl;
 
+	return match;
+}
+
+cv::Mat makeDrawing(cv::Mat image, std::string drawingName, bool doResize, cv::Scalar color) {
+	// Resize large image to fit screen
+	cv::resize(image, image, cv::Size(), 0.15, 0.15);
+
+	// Converting to grayscale
 	cv::Mat grayImage;
 	cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
 
+	// Thresholding image
 	cv::Mat threshImage;
 	cv::adaptiveThreshold(grayImage, threshImage, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 11);
 
+	// Final touches setting up image for contouring
 	cv::bitwise_not(threshImage, threshImage);
-
 	cv::stackBlur(threshImage, threshImage, cv::Size(3, 3));
 
-	//cv::imshow(drawingName + " gray", grayImage);
-	//cv::imshow(drawingName + " thresh", threshImage);
-
+	// Creating contours and finding the largest
 	std::vector<std::vector<cv::Point>> contours;
 	cv::findContours(threshImage, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
 
-	cv::Mat drawing = cv::Mat::zeros(threshImage.size(), CV_8UC1);
-
 	int largestContourIdx = findLargestContour(contours);
 
-	cv::drawContours(drawing, contours, largestContourIdx, color, 8, cv::LINE_8);
+	cv::Mat drawing = cv::Mat::zeros(threshImage.size(), CV_8UC1);
 
-	cv::imshow(drawingName, drawing);
+	cv::drawContours(drawing, contours, largestContourIdx, color, LINE_THICKNESS, cv::LINE_8);
+
+	if (drawingName != "") {
+		cv::imshow(drawingName, drawing);
+	}
 
 	return drawing;
 }
 
-bool isMatch(cv::Mat drawing1, cv::Mat drawing2, float threshold) {
-	float match = cv::matchShapes(drawing1, drawing2, cv::CONTOURS_MATCH_I2, 0);
-	std::cout << "Match (0-1): " << match << std::endl;
-
-	return match < threshold;
-}
-
-int main() {
-	std::cout << "Hello OpenCV " << CV_VERSION << std::endl;
+int main(int argc, char* argv[]) {
+	//RUN_VERBOSE = argc > 1 && argv[1] == "-v";
 
 	std::string basePath = "images/base/";
 	std::string gesturesPath = "images/gestures/";
 
-	// Base images for comparison
+	// Base images for later comparison
 	cv::Mat rock, paper, scissors;
-
-	std::cout << "Reading " << basePath << "paper.jpg" << std::endl;
 
 	cv::Mat basePaper = cv::imread(basePath + "paper.jpg", cv::IMREAD_COLOR);
 	cv::Mat baseRock = cv::imread(basePath + "rock.jpg", cv::IMREAD_COLOR);
 	cv::Mat baseScissors = cv::imread(basePath + "scissors.jpg", cv::IMREAD_COLOR);
 
-	// Checking for reading error
-	if (basePaper.empty()) {
-		std::cout << "Could not open or find the image paper" << std::endl;
+
+	if (RUN_VERBOSE) {
+		// Checking for reading errors
+		if (basePaper.empty()) {
+			std::cout << "Could not open or find the image paper" << std::endl;
+		}
+		if (baseRock.empty()) {
+			std::cout << "Could not open or find the image rock" << std::endl;
+		}
+		if (baseScissors.empty()) {
+			std::cout << "Could not open or find the image scissors" << std::endl;
+		}
 	}
-	if (baseRock.empty()) {
-		std::cout << "Could not open or find the image rock" << std::endl;
+
+	cv::Mat paperDrawing = makeDrawing(basePaper, "", false, cv::Scalar(255, 0, 0));
+	cv::Mat rockDrawing = makeDrawing(baseRock, "", false, cv::Scalar(255, 0, 0));
+	cv::Mat scissorsDrawing = makeDrawing(baseScissors, "", false, cv::Scalar(255, 0, 0));
+
+	if (RUN_VERBOSE) {
+		cv::imshow("paperDrawing", paperDrawing);
+		cv::imshow("rockDrawing", rockDrawing);
+		cv::imshow("scissorsDrawing", scissorsDrawing);
+
+		cv::waitKey(0);
 	}
-	if (baseScissors.empty()) {
-		std::cout << "Could not open or find the image scissors" << std::endl;
+
+	int maxNones = 0;
+	int maxPapers = 0;
+	int maxRocks = 0;
+	int maxScissors = 0;
+
+	// Trackers for success rates
+	int initialNoneSuccessRate = 0;
+	int initialPaperSuccessRate = 0;
+	int initialRockSuccessRate = 0;
+	int initialScissorsSuccessRate = 0;
+
+	std::vector<cv::Mat> realNonesVector;
+	std::vector<cv::Mat> realPapersVector;
+	std::vector<cv::Mat> realRocksVector;
+	std::vector<cv::Mat> realScissorsVector;
+
+	std::vector<std::string> falseIds;
+
+	// Trackers for false positives
+	int falseNone = 0;
+	int falsePaper = 0;
+	int falseRock = 0;
+	int falseScissors = 0;
+
+	int readingErrors = 0;
+
+	if (!fs::exists(gesturesPath) && fs::is_directory(gesturesPath)) {
+		std::cout << "Could not open or find the directory gestures" << std::endl;
+
+		return -1;
 	}
 
-	cv::resize(basePaper, basePaper, cv::Size(), 0.15, 0.15);
-	cv::resize(baseRock, baseRock, cv::Size(), 0.15, 0.15);
-	cv::resize(baseScissors, baseScissors, cv::Size(), 0.15, 0.15);
+	for (const auto& entry : fs::directory_iterator(gesturesPath)) {
+		std::string fileName = entry.path().filename().string();
+		std::string directory = entry.path().parent_path().filename().string();
 
-	// Converting to grayscale
-	cv::Mat grayPaper, grayRock, grayScissors;
+		Gesture properMatch = fileName.substr(0, fileName.find("_")) == "paper"
+			? Gesture::PAPER
+			: fileName.substr(0, fileName.find("_")) == "rock"
+			? Gesture::ROCK
+			: fileName.substr(0, fileName.find("_")) == "scissors"
+			? Gesture::SCISSORS
+			: Gesture::NONE;
 
-	cv::cvtColor(basePaper, grayPaper, cv::COLOR_BGR2GRAY);
-	cv::cvtColor(baseRock, grayRock, cv::COLOR_BGR2GRAY);
-	cv::cvtColor(baseScissors, grayScissors, cv::COLOR_BGR2GRAY);
 
-	// Thresholding images
-	cv::Mat threshPaper, threshRock, threshScissors;
+		if (properMatch == Gesture::PAPER) {
+			maxPapers++;
+		}
+		else if (properMatch == Gesture::ROCK) {
+			maxRocks++;
+		}
+		else if (properMatch == Gesture::SCISSORS) {
+			maxScissors++;
+		}
+		else {
+			maxNones++;
+		}
 
-	cv::adaptiveThreshold(grayPaper, threshPaper, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 11);
-	cv::adaptiveThreshold(grayRock, threshRock, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 11);
-	cv::adaptiveThreshold(grayScissors, threshScissors, 255, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 11, 11);
+		cv::Mat gesture = cv::imread(entry.path().string(), cv::IMREAD_COLOR);
 
-	// Final touches setting up images for contouring
-	cv::bitwise_not(threshPaper, threshPaper);
-	cv::bitwise_not(threshRock, threshRock);
-	cv::bitwise_not(threshScissors, threshScissors);
+		// Checking for reading errors
+		if (gesture.empty()) {
+			std::cout << "Could not open or find the image (Reading)" << std::endl;
+			readingErrors++;
 
-	cv::stackBlur(threshPaper, threshPaper, cv::Size(3, 3));
-	cv::stackBlur(threshRock, threshRock, cv::Size(3, 3));
-	cv::stackBlur(threshScissors, threshScissors, cv::Size(3, 3));
+			continue;
+		}
 
-	// Creating contours
-	std::vector<std::vector<cv::Point>> paperContours, rockContours, scissorsCotours;
+		cv::Mat gestureDrawing = makeDrawing(gesture, "", true, cv::Scalar(255, 0, 0));
 
-	cv::findContours(threshPaper, paperContours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-	cv::findContours(threshRock, rockContours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-	cv::findContours(threshScissors, scissorsCotours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+		// Determining matches to base images and the closest match among them
+		double matchPaper = matchingOf(gestureDrawing, paperDrawing);
+		double matchRock = matchingOf(gestureDrawing, rockDrawing);
+		double matchScissors = matchingOf(gestureDrawing, scissorsDrawing);
+		double smallestMatch = std::min(matchPaper, std::min(matchRock, matchScissors));
 
-	cv::Mat paperDrawing = cv::Mat::zeros(threshPaper.size(), CV_8UC1);
-	cv::Mat rockDrawing = cv::Mat::zeros(threshRock.size(), CV_8UC1);
-	cv::Mat scissorsDrawing = cv::Mat::zeros(threshScissors.size(), CV_8UC1);
+		if (smallestMatch > ACCEPTANCE_THRESHOLD) {
+			if (RUN_VERBOSE) {
+				std::cout << "Match! " << entry.path().filename() << " -> " << "none (" << smallestMatch << ")" << std::endl;
+			}
 
-	int largPaperCIndex = findLargestContour(paperContours);
-	int largRockCIndex = findLargestContour(rockContours);
-	int largScissorsCIndex = findLargestContour(scissorsCotours);
+			if (properMatch == Gesture::NONE) {
+				initialNoneSuccessRate++;
+				realNonesVector.push_back(gestureDrawing);
+			}
+			else {
+				falseNone++;
+				falseIds.push_back(fileName);
+			}
+		}
+		else if (smallestMatch == matchPaper) {
+			if (RUN_VERBOSE) {
+				std::cout << "Match! " << entry.path().filename() << " -> " << "paper (" << smallestMatch << ")" << std::endl;
+			}
 
-	cv::drawContours(paperDrawing, paperContours, largPaperCIndex, cv::Scalar(255, 0, 0), 8, cv::LINE_8);
-	cv::drawContours(rockDrawing, rockContours, largRockCIndex, cv::Scalar(255, 0, 0), 8, cv::LINE_8);
-	cv::drawContours(scissorsDrawing, scissorsCotours, largScissorsCIndex, cv::Scalar(255, 0, 0), 8, cv::LINE_8);
+			if (properMatch == Gesture::PAPER) {
+				initialPaperSuccessRate++;
+				realPapersVector.push_back(gestureDrawing);
+			}
+			else {
+				falsePaper++;
+				falseIds.push_back(fileName);
+			}
+		}
+		else if (smallestMatch == matchRock) {
+			if (RUN_VERBOSE) {
+				std::cout << "Match! " << entry.path().filename() << " -> " << "rock (" << smallestMatch << ")" << std::endl;
+			}
 
-	cv::imshow("paperDrawing", paperDrawing);
-	cv::imshow("rockDrawing", rockDrawing);
-	cv::imshow("scissorsDrawing", scissorsDrawing);
+			if (properMatch == Gesture::ROCK) {
+				initialRockSuccessRate++;
+				realRocksVector.push_back(gestureDrawing);
+			}
+			else {
+				falseRock++;
+				falseIds.push_back(fileName);
+			}
+		}
+		else if (smallestMatch == matchScissors) {
+			if (RUN_VERBOSE) {
+				std::cout << "Match! " << entry.path().filename() << " -> " << "scissors (" << smallestMatch << ")" << std::endl;
+			}
+
+			if (properMatch == Gesture::SCISSORS) {
+				initialScissorsSuccessRate++;
+				realScissorsVector.push_back(gestureDrawing);
+			}
+			else {
+				falseScissors++;
+				falseIds.push_back(fileName);
+			}
+		}
+
+		std::cout << std::endl;
+	}
+
+	std::cout << "--------------------------------------------------" << std::endl;
+
+	int endNoneSuccessRate = initialNoneSuccessRate;
+	int endPaperSuccessRate = initialPaperSuccessRate;
+	int endRockSuccessRate = initialRockSuccessRate;
+	int endScissorsSuccessRate = initialScissorsSuccessRate;
+
+	// If there are false positives, check them again until none remains, compare them to proven real matches
+	while (!falseIds.empty()) {
+		if (MAX_RETRIES == 0) {
+			std::cout << std::endl << "Max retries reached, exiting!" << std::endl;
+
+			break;
+		}
+
+		for (int i = 0; i < falseIds.size(); i++) {
+			std::string falseId = falseIds[i];
+
+			cv::Mat falseGesture = cv::imread(gesturesPath + falseId, cv::IMREAD_COLOR);
+
+			Gesture properMatch = falseId.substr(0, falseId.find("_")) == "paper"
+				? Gesture::PAPER
+				: falseId.substr(0, falseId.find("_")) == "rock"
+				? Gesture::ROCK
+				: falseId.substr(0, falseId.find("_")) == "scissors"
+				? Gesture::SCISSORS
+				: Gesture::NONE;
+
+
+			if (RUN_VERBOSE) {
+				// Checking for reading error
+				if (falseGesture.empty()) {
+					std::cout << "Could not open or find the image for reading" << std::endl;
+					readingErrors++;
+
+					continue;
+				}
+			}
+
+			cv::Mat falseGestureDrawing = makeDrawing(falseGesture, "", true, cv::Scalar(255, 0, 0));
+
+			// Determining matches to base images and the closest match among them
+			if (properMatch == Gesture::PAPER) {
+				double papersMatch = 1;
+
+				for (int i = 0; i < realPapersVector.size(); i++) {
+					double match = matchingOf(falseGestureDrawing, realPapersVector[i]);
+
+					papersMatch = match < papersMatch ? match : papersMatch;
+				}
+
+				if (papersMatch <= ACCEPTANCE_THRESHOLD) {
+					std::cout << "Match! " << falseId << " -> " << "paper" << std::endl;
+
+					endPaperSuccessRate++;
+					realPapersVector.push_back(falseGestureDrawing);
+					falseIds.erase(falseIds.begin() + i);
+				}
+			}
+
+			if (properMatch == Gesture::ROCK) {
+				double rocksMatch = 1;
+
+				for (int i = 0; i < realRocksVector.size(); i++) {
+					double match = matchingOf(falseGestureDrawing, realRocksVector[i]);
+
+					rocksMatch = match < rocksMatch ? match : rocksMatch;
+				}
+
+				if (rocksMatch <= ACCEPTANCE_THRESHOLD) {
+					std::cout << "Match! " << falseId << " -> " << "rock" << std::endl;
+
+					endRockSuccessRate++;
+					realRocksVector.push_back(falseGestureDrawing);
+					falseIds.erase(falseIds.begin() + i);
+				}
+			}
+
+			if (properMatch == Gesture::SCISSORS) {
+				double scissorsMatch = 1;
+
+				for (int i = 0; i < realScissorsVector.size(); i++) {
+					double match = matchingOf(falseGestureDrawing, realScissorsVector[i]);
+
+					scissorsMatch = match < scissorsMatch ? match : scissorsMatch;
+				}
+
+				if (scissorsMatch <= ACCEPTANCE_THRESHOLD) {
+					std::cout << "Match! " << falseId << " -> " << "scissors" << std::endl;
+
+					endScissorsSuccessRate++;
+					realScissorsVector.push_back(falseGestureDrawing);
+					falseIds.erase(falseIds.begin() + i);
+				}
+			}
+
+			if (properMatch == Gesture::NONE) {
+				double nonesMatch = 1;
+
+				for (int i = 0; i < realNonesVector.size(); i++) {
+					double match = matchingOf(falseGestureDrawing, realNonesVector[i]);
+
+					nonesMatch = match < nonesMatch ? match : nonesMatch;
+				}
+
+				if (nonesMatch <= ACCEPTANCE_THRESHOLD) {
+					std::cout << "Match! " << falseId << " -> " << "none" << std::endl;
+
+					endNoneSuccessRate++;
+					realNonesVector.push_back(falseGestureDrawing);
+					falseIds.erase(falseIds.begin() + i);
+				}
+			}
+		}
+
+		std::cout << "Remaining false positives: " << falseIds.size() << std::endl;
+
+		MAX_RETRIES--;
+	}
+
+	std::cout << std::endl << "--------------------------------------------------" << std::endl;
+	std::cout << "Initial none success rate: " << initialNoneSuccessRate << "/" << maxNones << std::endl;
+	std::cout << "Initial paper success rate: " << initialPaperSuccessRate << "/" << maxPapers << std::endl;
+	std::cout << "Initial rock success rate: " << initialRockSuccessRate << "/" << maxRocks << std::endl;
+	std::cout << "Initial scissors success rate: " << initialScissorsSuccessRate << "/" << maxScissors << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "End none success rate: " << initialNoneSuccessRate << "/" << maxNones << std::endl;
+	std::cout << "End paper success rate: " << initialPaperSuccessRate << "/" << maxPapers << std::endl;
+	std::cout << "End rock success rate: " << initialRockSuccessRate << "/" << maxRocks << std::endl;
+	std::cout << "End scissors success rate: " << initialScissorsSuccessRate << "/" << maxScissors << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;
+	std::cout << "False positives for none: " << falseNone << std::endl;
+	std::cout << "False positives for paper: " << falsePaper << std::endl;
+	std::cout << "False positives for rock: " << falseRock << std::endl;
+	std::cout << "False positives for scissors: " << falseScissors << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;
+
+	std::cout << std::endl << "--------------------------------------------------" << std::endl;
+	std::cout << "Initial success rate: "
+		<< (initialNoneSuccessRate + initialPaperSuccessRate + initialRockSuccessRate + initialScissorsSuccessRate)
+		<< "/"
+		<< (maxNones + maxPapers + maxRocks + maxScissors) << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;
+
+	std::cout << std::endl << "--------------------------------------------------" << std::endl;
+	std::cout << "End success rate: "
+		<< (endNoneSuccessRate + endPaperSuccessRate + endRockSuccessRate + endScissorsSuccessRate)
+		<< "/"
+		<< (maxNones + maxPapers + maxRocks + maxScissors) << std::endl;
+	std::cout << "--------------------------------------------------" << std::endl;
 
 	cv::waitKey(0);
-
-
-
-	//double match1 = cv::matchShapes(paperDrawing, rockDrawing, cv::CONTOURS_MATCH_I2, 0);
-	//std::cout << "Match (0-1): " << match1 << std::endl;
-
-	//std::string result1 = match1 < .15 ? "It's a match!" : "They aren't the same!";
-	//std::cout << result1 << std::endl;
-
-	//std::string result2 = match2 < .15 ? "It's a match!" : "They aren't the same!";
-	//std::cout << result2 << std::endl;
-
-	cv::waitKey(0);
-
-	std::cout << "Bye OpenCV" << std::endl;
 
 	return 0;
 }
